4b8f8693217a0f326b8eb6d3875f0ecac26b08b8
Rutuja1427 <rutupatil5199@gmail.com>
2025-03-20 01:22:16 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config({});
 
 const app = express();
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +18,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
